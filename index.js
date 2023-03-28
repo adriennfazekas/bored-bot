c1106aa0c37238cd435cdec60a19be3f7a32420e
Fazekas Adrienn <adreeja17@gmail.com>
2023-03-28 23:21:21 +0200

    created the button that give new activity

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
-fetch("https://apis.scrimba.com/bored/api/activity")
+document.getElementById("btn").addEventListener("click", function() {
+    fetch("https://apis.scrimba.com/bored/api/activity")
     .then(response => response.json())
     .then(data => {
-        // console.log(data)
-        document.getElementById("container").innerHTML = data.activity
+        document.getElementById("activity").textContent = data.activity
     })
+})
+
